09841e247e7881b0d04b8672f69a83f9f59ebc10
张郑樟 <zhangzhengzhang@sunlands.com>
2018-07-17 17:39:13 +0800

    Add rl demo

---
+++ rl/treasure_on_dangerous_island/run_this.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+"""
+Filename: run_this.py
+Function:
+Author: zhangzhengzhang@sunlands.com
+Create: 2018/2/27 14:01
+
+"""
+import os
+import time
+import numpy as np
+
+class Island(object):
+    def __init__(self):
+        self.states = [1, 2, 3, 4, 5, 6, 7, 8]
+        self.terminal_states = {6, 7, 8}
+        self.actions = ['n', 'e', 's', 'w']
+        self.rewards ={
+            (1, 's'): -1.0,
+            (3, 's'): 1.0,
+            (5, 's'): -1.0,
+        }
+        self.env_background = ["_"] * len(self.states)
+        self.skeletons_is_there = [6, 8]
+        self.treasure_is_there = 7
+        self.trans = {
+            (1, 's'): 6,
+            (1, 'e'): 2,
+            (2, 'w'): 1,
+            (2, 'e'): 3,
+            (3, 's'): 7,
+            (3, 'w'): 2,
+            (3, 'e'): 4,
+            (4, 'w'): 3,
+            (4, "e"): 5,
+            (5, 's'): 8,
+            (5, 'w'): 4,
+        }
+        self.info_dict = {
+            6: 'You died!',
+            7: 'You got treasure!',
+            8: 'You died!',
+        }
+
+    def reset(self):
+        self.mark_is_here = 1
+
+    def render(self):
+        env_to_show = self.env_background[:]
+        def offset_replace(state, icon):
+            env_to_show[state - 1] = icon
+        for skeleton in self.skeletons_is_there:
+            offset_replace(skeleton, 'X')
+        offset_replace(self.treasure_is_there, 'T')
+        offset_replace(self.mark_is_here, 'O')
+        cmd = "cls"
+        os.system(cmd)
+        for i in range(5):
+            print env_to_show[i],
+        print ''
+        for i in range(5, 7):
+            print env_to_show[i], 'W',
+        print env_to_show[7]
+
+
+
+    def step(self, action):
+        key = (self.mark_is_here, action)
+        next_state = self.mark_is_here
+        reward = 0
+        info = ''
+        done = False
+        if key in self.trans:
+            next_state = self.trans[key]
+            self.mark_is_here = next_state
+        else:
+            info = 'You didn\'t move!'
+        if key in self.rewards:
+            reward = self.rewards[key]
+        if next_state in self.terminal_states:
+            done = True
+        if next_state in self.info_dict:
+            info = self.info_dict[next_state]
+        return next_state, reward, done, info
+
+
+
+
+def random_policy():
+    island = Island()
+    island.reset()
+    island.render()
+    done = False
+    while not done:
+        time.sleep(0.3)
+        action_name = np.random.choice(island.actions)
+        state, reward, done, info = island.step(action_name)
+        island.render()
+        print info
+    # island.render()
+# random_policy()
+
+def sarsa():
+    island = Island()
+    ACTIONS = island.actions
+    STATES = island.states
+    import pandas as pd
+    def build_q_table(n_states, actions):
+        table = pd.DataFrame(
+            np.zeros((n_states, len(actions))),
+            columns=actions,
+        )
+        return table
+    q_table = build_q_table( len(STATES) + 1, ACTIONS )
+
+    def choose_action(state, q_table):
+        state_actions = q_table.iloc[state, :]
+        # exploration and exploitation trade-off
+        if (np.random.uniform() > 0.9) or ((state_actions == 0).all()):
+            action_name = np.random.choice(ACTIONS)
+        else:
+            action_name = state_actions.idxmax()
+        return action_name
+    for episode in range(20):
+        step_counter = 0
+        done = False
+        island.reset()
+        S = 1
+        island.render()
+        A = choose_action(S, q_table)
+        while not done:
+           S_, R, done, info = island.step(A)
+           q_predict = q_table.loc[S, A]
+           if done:
+               q_targe = R
+           else:
+               A_ = choose_action(S_, q_table)
+               q_targe = R + 0.9 * q_table.loc[S_, A_]
+           q_table.loc[S, A] += 0.1 * (q_targe - q_predict)
+           S = S_
+           A = A_
+           island.render()
+           print info
+           step_counter += 1
+           time.sleep(0.3)
+        interaction = 'Episode %s: total_steps = %s, got = %s' % (episode + 1, step_counter, R)
+        print interaction
+        time.sleep(1)
+
+    return q_table
+
+
+sarsa()
---
+++ rl/treasure_on_the_right/treasure_on_the_right.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+"""
+Filename: treasure_on_the_right.py
+Function:
+Author: zhangzhengzhang@sunlands.com
+Create: 2018/2/26 15:11
+
+"""
+import numpy as np
+import pandas as pd
+import time
+
+# np.random.seed(2)
+
+N_STATES = 6
+# N_STATES = 10
+ACTIONS = ['left', 'right']
+EPSILON = 0.9
+ALPHA = 0.1
+GAMMA = 0.9
+MAX_EPISODES = 13
+FRESH_TIME = 0.3
+
+def build_q_table(n_states, actions):
+    table = pd.DataFrame(
+        np.zeros((n_states, len(actions))),
+        columns=actions,
+    )
+    return table
+
+def choose_action(state, q_table):
+    state_actions = q_table.iloc[state, :]
+    # exploration and exploitation trade-off
+    if (np.random.uniform() > EPSILON) or ((state_actions == 0).all()):
+        action_name = np.random.choice(ACTIONS)
+    else:
+        action_name = state_actions.idxmax()
+    return action_name
+
+def get_env_feedback(S, A):
+    if A == 'right':
+        if S == N_STATES - 2:
+            S_ = 'terminal'
+            R = 1
+        else:
+            S_ = S + 1
+            R = 0
+    else:
+        R = 0
+        if S == 0:
+            S_ = S
+        else:
+            S_ = S - 1
+    return S_, R
+
+def update_env(S, episode, step_counter):
+    env_list = ['-']*(N_STATES-1) + ['T']
+    if S == 'terminal':
+        interaction = 'Episode %s: total_steps = %s' % (episode + 1, step_counter)
+        print '\r{}'.format(interaction),
+        time.sleep(2)
+        print '\r                                ',
+    else:
+        env_list[S] = 'o'
+        interaction = ''.join(env_list)
+        print '\r{}'.format(interaction),
+        time.sleep(FRESH_TIME)
+
+def random_policy():
+    avg_step = 0
+    for episode in range(MAX_EPISODES):
+        step_counter = 0
+        S = 0
+        is_terminated = False
+        update_env(S, episode, step_counter)
+        while not is_terminated:
+            A = np.random.choice(ACTIONS)
+            S_, _ = get_env_feedback(S, A)
+            if S_ == 'terminal':
+                is_terminated = True
+            S = S_
+            update_env(S, episode, step_counter +1)
+            step_counter += 1
+        avg_step += step_counter
+    avg_step /= MAX_EPISODES * 1.0
+    print 'avg_step: %s' % avg_step
+
+def rl_q_learning():
+    avg_step = 0
+    q_table = build_q_table(N_STATES, ACTIONS)
+    for episode in range(MAX_EPISODES):
+        step_counter = 0
+        S = 0
+        is_terminated = False
+        update_env(S, episode, step_counter)
+        while not is_terminated:
+            A = choose_action(S, q_table)
+            S_, R = get_env_feedback(S, A)
+            q_predict = q_table.loc[S, A]
+            if S_ != 'terminal':
+                q_target = R + GAMMA * q_table.loc[S_, :].max()
+            else:
+                q_target = R
+                is_terminated = True
+            q_table.loc[S, A] += ALPHA * (q_target - q_predict)
+            S = S_
+            update_env(S, episode, step_counter + 1)
+            step_counter += 1
+        avg_step += step_counter
+    avg_step /= MAX_EPISODES * 1.0
+    print 'avg_step: %s' % avg_step
+    return q_table
+
+def rl_sarsa():
+    avg_step = 0
+    q_table = build_q_table(N_STATES, ACTIONS)
+    for episode in range(MAX_EPISODES):
+        step_counter = 0
+        S = 0
+        is_terminated = False
+        update_env(S, episode, step_counter)
+        A = choose_action(S, q_table)
+        while not is_terminated:
+            S_, R = get_env_feedback(S, A)
+            q_predict = q_table.loc[S, A]
+            if S_ != 'terminal':
+                A_ = choose_action(S_, q_table)
+                q_target = R + GAMMA * q_table.loc[S_, A_]
+            else:
+                q_target = R
+                is_terminated = True
+            q_table.loc[S, A] += ALPHA * (q_target - q_predict)
+            S = S_
+            A = A_
+            update_env(S, episode, step_counter + 1)
+            step_counter += 1
+        avg_step += step_counter
+    avg_step /= MAX_EPISODES * 1.0
+    print 'avg_step: %s' % avg_step
+    return q_table
+
+def rl_sarsa_lambda():
+    avg_step = 0
+    q_table = build_q_table(N_STATES, ACTIONS)
+    eligibility_trace = q_table.copy()
+    for episode in range(MAX_EPISODES):
+        eligibility_trace *= 0
+        step_counter = 0
+        S = 0
+        is_terminated = False
+        update_env(S, episode, step_counter)
+        A = choose_action(S, q_table)
+        while not is_terminated:
+            S_, R = get_env_feedback(S, A)
+            q_predict = q_table.loc[S, A]
+            if S_ != 'terminal':
+                A_ = choose_action(S_, q_table)
+                q_target = R + GAMMA * q_table.loc[S_, A_]
+            else:
+                q_target = R
+                is_terminated = True
+            error = (q_target - q_predict)
+            # method 1:
+            eligibility_trace.loc[S, :] *= 0
+            eligibility_trace.loc[S, A] = 1
+            #
+            # # method 2:
+            # eligibility_trace.loc[S, A] = 1
+
+            # method 3:
+            # eligibility_trace.loc[S, A] += 1
+
+            q_table += ALPHA * error * eligibility_trace
+            eligibility_trace *= GAMMA * ALPHA
+            # eligibility_trace *= GAMMA * 1
+            # eligibility_trace *= GAMMA * 0
+            S = S_
+            A = A_
+            update_env(S, episode, step_counter + 1)
+            step_counter += 1
+        avg_step += step_counter
+    avg_step /= MAX_EPISODES * 1.0
+    print 'avg_step: %s' % avg_step
+    return q_table
+
+if __name__ == '__main__':
+    print "random policy:"
+    random_policy()
+    print "q-learning:"
+    q_table_q_learning = rl_q_learning()
+    print q_table_q_learning
+    print "sarsa:"
+    q_table_sarsa = rl_sarsa()
+    print q_table_sarsa
+    print "sara lambda:"
+    q_table_sarsa_lambda = rl_sarsa_lambda()
+    print q_table_sarsa_lambda
