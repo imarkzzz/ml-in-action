7eff449e6717f0522352338f6ac2be9d9ae5adc7
张郑樟 <zhangzhengzhang@sunlands.com>
2018-12-07 11:20:28 +0800

    feat: add chain of responsibility pattern

---
+++ design-pattern/src/chainofresponsibility/ErrorLogger.java
@@ -0,0 +1,16 @@
+package chainofresponsibility;
+
+/**
+ * Created by mark on 2018/12/7.
+ */
+public class ErrorLogger extends AbstractLogger {
+
+    public ErrorLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Error Console::Logger: " + message);
+    }
+}
