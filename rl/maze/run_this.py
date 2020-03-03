a4ae5a420534006ac28f6bc17d116b998d2c3a90
iMarkzzz <1126314679@qq.com>
2020-03-04 06:54:13 +0800

    feat: punish back

--- rl/maze/run_this.py
+++ rl/maze/run_this.py
@@ -128,6 +128,74 @@ def sarsa():
         print(interaction)
         time.sleep(1)
 
-random_policy()
+
+def qlearning2():
+    q_table = build_q_table(16, 4)
+    N_ACTIONS = 4
+    s_dict = {}
+    env = Maze()
+    def choose_action(state, q_table):
+        state_actions = q_table.iloc[state, :]
+        # exploration and exploitation trade-off
+        if (np.random.uniform() > 0.9) or ((state_actions == 0).all()):
+            action_name = np.random.choice(N_ACTIONS)
+        else:
+            action_name = state_actions.idxmax()
+        return action_name
+
+    def S2idx(S):
+        S_key = str(S)
+        if S_key in s_dict:
+            S_idx = s_dict[S_key]
+        else:
+            S_idx = len(s_dict)
+            s_dict[S_key] = S_idx
+        return S_idx
+
+    def check_hesitation(A, A_pre):
+        action_space = env.action_space
+        act_A = action_space[A]
+        act_A_pre = action_space[A_pre]
+        if act_A == 'u' and act_A_pre == 'd':
+            return True
+        if act_A == 'd' and act_A_pre == 'u':
+            return True
+        if act_A == 'l' and act_A_pre == 'r':
+            return True
+        if act_A == 'r' and act_A_pre == 'l':
+            return True
+        return False
+
+    for episode in range(200):
+        step_counter = 0
+        done = False
+        S = env.reset()
+        env.render()
+        A_pre = None
+        while not done:
+            A = choose_action(S2idx(S), q_table)
+            S_, R, done, info = env.step(A)
+            q_predict = q_table.loc[S2idx(S), A]
+            if not A_pre:
+                A_pre = A
+            if check_hesitation(A, A_pre):
+                R -= 0.5
+            A_pre == A
+            if done:
+                q_targe = R
+            else:
+                q_targe = R + 0.9 * q_table.loc[S2idx(S_), :].max()
+            q_table.loc[S2idx(S), A] += 0.1 * (q_targe - q_predict)
+            S = S_
+            env.render()
+            step_counter += 1
+            time.sleep(0.3)
+        interaction = 'Episode %s: total_steps = %s, got = %s' % (episode + 1, step_counter, R)
+        print(interaction)
+        time.sleep(1)
+
+# random_policy()
 # qlearning()
 # sarsa()
+
+qlearning2()
