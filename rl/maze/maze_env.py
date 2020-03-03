5a9092affca212fe7057bbc41c1426f37d66e348
iMarkzzz <1126314679@qq.com>
2020-03-03 12:04:31 +0800

    feat: support python3 only

--- rl/maze/maze_env.py
+++ rl/maze/maze_env.py
@@ -3,10 +3,7 @@
 import numpy as np
 import time
 import sys
-if sys.version_info.major == 2:
-    import Tkinter as tk
-else:
-    import tkinter as tk
+import tkinter as tk
 
 
 UNIT = 40   # pixels
