3ff180078d3391d4a72e2d4c2495491e6a48bdbd
iMarkzzz <1126314679@qq.com>
2020-03-05 07:14:37 +0800

    feat: list valid envs

--- rl/gym_example/liist_envs.py
+++ rl/gym_example/liist_envs.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 from gym import envs
+import gym
 envids = [spec.id for spec in envs.registry.all()]
+valid_envids = [
+
+]
+invalid_envids = [
+
+]
 for envid in sorted(envids):
-    print(envid)
+    try:
+        env = gym.make(envid)
+        valid_envids.append(envid)
+    except:
+        invalid_envids.append(envid)
+print(f"Valid env count: {len(valid_envids)}")
+print(valid_envids)
